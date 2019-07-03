5660cf1ff9954d7243c7ee04acef53b12e3f43fc
walkermj <walkermj.1984@gmail.com>
2019-07-03 13:40:38 +0100

    modified skeleton.R template file

--- templates/skeleton.R
+++ templates/skeleton.R
@@ -7,3 +7,18 @@ rm(list=ls(all=T))
 options(scipen = 200)
 
 #--Bring in required packages
+library(wrangleR)
+library(RPostgreSQL)
+library(lubridate)
+library(dplyr)
+
+#-------------------
+#--BRING IN DATA
+#-------------------
+
+#--Establish connection to PostGreSQL using RPostGreSQL package and read in data as data frame
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="db_name", host="127.0.0.1", port=5440, user='mwalker', password='password') #-- adapt to what is needed
+
+#-- bring in data needed for report from rer and cdm in MIS 
+df <- dbGetQuery(con, paste(readLines("folder/file.sql"), collapse = " "))
